92df49ea439eaba7c94b8960d8052986f700620c
Patrick Clary <pclary@outlook.com>
2013-10-05 15:01:38 -0700

    Added control loops

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,15 +1,16 @@
-//#include <AFMotor.h>
-//#include <SoftwareSerial.h>
+#include <AFMotor.h>
+#include <SoftwareSerial.h>
 #include "RingBuffer.h"
+#include <cmath>
 
 // Pin assignments
 const int LCDTx = 13;
 const int LCDRx = 13;
 
 // Initialize motors and LCD screen
-//AF_DCMotor rightMotor(3, MOTOR34_1KHZ);
-//AF_DCMotor leftMotor(4, MOTOR34_1KHZ);
-//SoftwareSerial screen = SoftwareSerial(LCDRx, LCDTx);
+AF_DCMotor rightMotor(3, MOTOR34_1KHZ);
+AF_DCMotor leftMotor(4, MOTOR34_1KHZ);
+SoftwareSerial screen = SoftwareSerial(LCDRx, LCDTx);
 
 // Encoder variables
 struct EncoderData
@@ -22,19 +23,31 @@ EncoderData rightData;
 EncoderData leftData;
 
 // Interrupt routines
+const int debounceMicros = 250;
+
 void rightPulse()
 {
-    rightData.pulseTimes.push(micros());
-    rightData.count++;
+    unsigned long time = micros();
+    
+    if (time - rightData.pulseTimes[0] > debounceMicros)
+    {
+        rightData.pulseTimes.push(time);
+        rightData.count++;
+    }
 }
 
 void leftPulse()
 {
-    leftData.pulseTimes.push(micros());
-    leftData.count++;
+    unsigned long time = micros();
+    
+    if (time - leftData.pulseTimes[0] > debounceMicros)
+    {
+        leftData.pulseTimes.push(time);
+        leftData.count++;
+    }
 }
 
-// Parameters
+// Physical parameters
 const float wheelDiameter = 0.1f; // meters
 const unsigned long pulsesPerRev = 6 * 25;
 const float ticksPerSecond = 1000000.f;
@@ -43,16 +56,87 @@ const float ticksPerSecond = 1000000.f;
 float getPosition(EncoderData& data);
 float getVelocity(EncoderData& data);
 
+// Command variables
+float rightPositionCommand;
+float rightVelocityCommand;
+float leftPositionCommand;
+float leftVelocityCommand;
+enum CommandMode
+{
+    positionMode,
+    velocityMode
+};
+CommandMode commandMode;
+
+// Integral terms
+const float integralThreshold = 255.f;
+float rightVelInt = 0.f;
+float leftVelInt = 0.f;
+
+// Tuning parameters
+float pl_kp = 1.f;
+float pl_ki = 0.f;
+float pl_kd = 0.f;
+float vl_kp = 1.f;
+float vl_ki = 1.f;
+float vl_kd = 0.f;
+
+// Loop control
+unsigned long lastMillis = 0;
+unsigned long loopPeriodMs = 50;
+
 
 void setup()
 {
     // Set up pins
     pinMode(LCDTx, OUTPUT);
+    
+    rightData.pulseTimes.push(0ul);
+    leftData.pulseTimes.push(0ul);
+    
+    commandMode = positionMode;
+    rightPositionCommand = 0.f;
+    leftPositionCommand = 0.f;
+    
+    attachInterrupt(0, &rightPulse, RISING);
+    attachInterrupt(1, &leftPulse, RISING);
 }
 
 
 void loop()
 {
+    if (commandMode == positionMode)
+    {   
+        // Position loop
+        rightVelocityCommand = pl_kp * (rightPositionCommand - getPosition(rightData));
+        leftVelocityCommand = pl_kp * (leftPositionCommand - getPosition(leftData));
+    }
+    
+    // Velocity loop
+    float rightVelErr = rightVelocityCommand - getVelocity(rightData);
+    float leftVelErr = leftVelocityCommand - getVelocity(leftData);
+    
+    rightVelInt += rightVelErr * (loopPeriodMs / 1000.f);
+    leftVelInt += leftVelErr * (loopPeriodMs / 1000.f);
+    
+    // Limit integral terms to prevent windup
+    rightVelInt = constrain(rightVelInt, -integralThreshold, integralThreshold);
+    leftVelInt = constrain(leftVelInt, -integralThreshold, integralThreshold);
+    
+    float rightAccel = vl_kp * rightVelErr + vl_ki * rightVelInt;
+    float leftAccel = vl_kp * leftVelErr + vl_ki * leftVelInt;
+    
+    rightMotor.run(rightAccel > 0.f ? FORWARD : BACKWARD);
+    rightMotor.setSpeed( (unsigned char)fabs(rightAccel) );
+    leftMotor.run(leftAccel > 0.f ? FORWARD : BACKWARD);
+    leftMotor.setSpeed( (unsigned char)fabs(leftAccel) );
+    
+    // Limit loop speed to a consistent value to make integration easier
+    while (millis() - lastMillis < loopPeriodMs)
+    {
+    }
+    
+    lastMillis = millis();
 }
 
 
